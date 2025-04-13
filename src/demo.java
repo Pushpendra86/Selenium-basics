c5c1e0f5ad3e8bd363116798ff774fade57dc806
Pushpendra <102226260+Pushpendra86@users.noreply.github.com>
2025-04-13 18:25:55 +0530

    init commit

--- src/demo.java
+++ src/demo.java
@@ -1,5 +1,6 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello, this is java");
+        System.out.println("hello this is again and updated");
     }
 }
